8afcb920f19184c87479c4ebc5529999ccc33a77
user <user@WIN-349MCH6R7UR>
2022-12-23 17:48:38 +0300

    Добавьте файлы проекта.

---
+++ 8ZAd Forms/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8ZAd_Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {        
+            string s = textBox1.Text;
+
+            Regex regex = new Regex(@"[-.?!);(,:]");
+            MatchCollection matches = regex.Matches(s);
+            var newText = Regex.Replace(s, "[-.?!);(,:]", "");
+            textBox2.Text += newText;
+            if (matches.Count == 0)
+            {
+                textBox2.Text = null;
+                MessageBox.Show("В предложении нет знаков препинания", "Error");
+
+            }
+        }
+    }
+}
---
+++ 8Zad/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _8Zad
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите текст");
+            string s = Console.ReadLine();
+            Regex regex = new Regex(@"[-.?!);(,:]");
+            MatchCollection matches = regex.Matches(s);
+            Console.WriteLine();
+            if (matches.Count == 0)
+            {
+                Console.WriteLine("Ошибка строка не может быть пустой");
+                return;
+            }
+
+            var newText = Regex.Replace(s, "[-.?!);(,:]", "");
+            Console.WriteLine("Исправленный текст: ");
+            Console.WriteLine(newText);
+                Console.ReadLine();
+            
+        }
+    }
+}
